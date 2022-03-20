ff4c9cf4c8f4925c0499ca68c1c7c608b6224149
pratiktamhane <96224999+pratiktamhane@users.noreply.github.com>
2022-03-20 19:36:23 +0530

    Counter-Timer

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+  // write your code here
+  const [input, setinputt] = useState();
+
+  setInterval(() => {
+    if (input > 0) {
+      setinputt(parseInt(input) - 1);
+    }
+  }, 1000);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyDown={(e) => {
+              if (e.keyCode == 13) {
+                setinputt(e.target.value);
+              }
+            }}
+          />
+          {""}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">
+        {/* remaining time */ input < 6 ? input : null}
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
